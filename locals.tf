4b425ec2fede797b516dabc4ed30ca36dacd6098
Varun <varunkumar.pinnam@gmail.com>
2026-03-11 06:13:24 +0530

    vpC

--- locals.tf
+++ locals.tf
@@ -13,4 +13,14 @@ vpc_final_tags = merge(
     var.vpc_tags
     )
 
+gw_final_tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    },
+    var.gw_tags
+    )
+
 }
+
+
