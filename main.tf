8206c2afb7978888b1e402d8b496ecdeec9a21da
Varun <varunkumar.pinnam@gmail.com>
2026-03-13 15:47:12 +0530

    vpC

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public[count.index]
 
   tags = local.aws_natgw_final_tags
 
