0fc49496757bc3b741de1da3bd18d3860a46d8dd
Varun <varunkumar.pinnam@gmail.com>
2026-03-14 05:28:39 +0530

    vpC

--- peering.tf
+++ peering.tf
@@ -14,10 +14,10 @@ resource "aws_vpc_peering_connection" "default" {
   }
 }
 
-/* #aws route for public subnet
+#aws route for public subnet
 resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
-  vpc_peering_connection_id  = 
-} */
+  vpc_peering_connection_id  = data.aws_vpc.default_vpc.cidr_block
+}
